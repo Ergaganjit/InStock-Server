c075d8d70dec37c19fd4d774f628793b6e01f798
ergaganjit <er.gaganjit.kaur@gmail.com>
2024-05-22 15:25:06 -0700

    Added api to delete warehouse

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -2,7 +2,7 @@
 const knex = require("knex")(require("../knexfile"));
 
 
-
+//get all warehouse
 const getWarehouses=async (_req, res) => {
     try {
       const data = await knex("warehouses");
@@ -15,6 +15,31 @@ const getWarehouses=async (_req, res) => {
     }
   };
 
+
+//delete warehouse
+const removeWarehouse = async (req, res) => {
+    try {
+      const rowsDeleted = await knex("warehouses")
+        .where({ id: req.params.id })
+        .delete();
+  
+      if (rowsDeleted === 0) {
+        return res
+          .status(404)
+          .json({ message: `warehouse with ID ${req.params.id} not found` });
+      }
+  
+      // No Content response
+      res.sendStatus(204);
+    } catch (error) {
+      res.status(500).json({
+        message: `Unable to delete warehouse: ${error}`
+      });
+    }
+  };
+  
+
   module.exports={   
-    getWarehouses
+    getWarehouses,
+    removeWarehouse
   }
