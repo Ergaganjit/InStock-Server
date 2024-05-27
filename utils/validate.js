da4713c2e14ee13a9fc4720a9845176f8a540923
ergaganjit <er.gaganjit.kaur@gmail.com>
2024-05-26 19:53:39 -0700

    Edit updated

--- utils/validate.js
+++ utils/validate.js
@@ -2,38 +2,34 @@ const knex = require("knex")(require("../knexfile"));
 
 
 const validateUpdateInventory = async (req, res) => {
-  const { warehouse_name, item_name, description, category, status, quantity } = req.body;
-  const { id } = req.params;
-
-  // Check if all required fields are present
-  if (!warehouse_name || !item_name || !description || !category || !status || !quantity) {
-    return { status: 400, message: 'All fields are required' };
-  }
-
-  // Check if quantity is a number
-  if (status === 'In Stock' && (quantity === undefined || isNaN(quantity))) {
-    return { status: 400, message: 'Quantity must be a number' };
-  }
-
-  try {
-    // Check if the inventory ID exists
-    const inventory = await knex('inventories').where({ id }).first();
-    if (!inventory) {
-      return { status: 404, message: 'Inventory ID not found' };
+    const { warehouse_id, item_name, description, category, status, quantity } = req.body;
+    const { id } = req.params;
+  
+    if (!warehouse_id || !item_name || !description || !category || !status) {
+      return { status: 400, message: 'All fields are required' };
     }
-
-    // Check if the warehouse name exists and get the warehouse ID
-    const warehouse = await knex('warehouses').where({ warehouse_name }).first();
-    if (!warehouse) {
-      return { status: 400, message: 'Invalid warehouse name' };
+  
+    if (status === 'In Stock' && (quantity === undefined || isNaN(quantity))) {
+      return { status: 400, message: 'Quantity must be a number and present when status is In Stock' };
     }
-
-    return { status: 200, warehouseId: warehouse.id };
-  } catch (error) {
-    console.error(error);
-    return { status: 500, message: 'An error occurred during validation' };
-  }
-};
+  
+    try {
+      const inventory = await knex('inventories').where({ id }).first();
+      if (!inventory) {
+        return { status: 404, message: 'Inventory ID not found' };
+      }
+  
+      const warehouse = await knex('warehouses').where({ id: warehouse_id }).first();
+      if (!warehouse) {
+        return { status: 400, message: 'Invalid warehouse ID' };
+      }
+  
+      return { status: 200, warehouseId: warehouse.id };
+    } catch (error) {
+      console.error(error);
+      return { status: 500, message: 'An error occurred during validation' };
+    }
+  };
 
 const validateNewInventoryData = async (data) => {
     const { warehouse_id, item_name, description, category, status, quantity } = data;
